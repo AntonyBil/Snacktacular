99d886e14fab0f1c3506a302522a54e4b200072c
AntonyBil <beletskiy.ant@gmail.com>
2023-10-31 21:51:11 +0200

    Initial commit add firebese ang gitignore file

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,30 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by apple on 31.10.2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct SnacktacularApp: App {
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
