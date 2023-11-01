c1903eb5cb2319f3377284b4d12a446558d32141
AntonyBil <beletskiy.ant@gmail.com>
2023-11-01 22:16:54 +0200

    add registr and login functions

--- Snacktacular/LoginView.swift
+++ Snacktacular/LoginView.swift
@@ -12,6 +12,9 @@ struct LoginView: View {
     
     @State private var email = ""
     @State private var password = ""
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    
     
     var body: some View {
         NavigationStack {
@@ -23,7 +26,13 @@ struct LoginView: View {
                 
                 Group {
                     TextField("E-mail", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .submitLabel(.next)
                     SecureField("Password", text: $password)
+                        .textInputAutocapitalization(.never)
+                        .submitLabel(.done)
                 }
                 .textFieldStyle(.roundedBorder)
                 .overlay {
@@ -34,7 +43,7 @@ struct LoginView: View {
                 
                 HStack {
                     Button {
-                        //
+                        register()
                     } label: {
                         Text("Sing Up")
                     }
@@ -42,7 +51,7 @@ struct LoginView: View {
                     
                     
                     Button {
-                        //
+                        login()
                     } label: {
                         Text("Log In")
                     }
@@ -53,9 +62,38 @@ struct LoginView: View {
                 .tint(Color("SnackColor"))
                 .font(.title2)
                 .padding(.top)
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .alert(alertMessage, isPresented: $showingAlert) {
+                Button("OK", role: .cancel) {}
             }
         }
-        
+    }
+    
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error { //login error ocured
+                print("Registration Error: \(error.localizedDescription)")
+                alertMessage = "Login Error: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("Registration success!")
+                //TODO: loadListView
+            }
+        }
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error { //login error ocured
+                print("Login Error: \(error.localizedDescription)")
+                alertMessage = "Login Error: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("Login success!")
+                //TODO: loadListView
+            }
+        }
     }
 }
 
