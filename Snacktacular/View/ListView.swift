ac59c99d17f50914168f4f9fd29352d9378e9e43
AntonyBil <beletskiy.ant@gmail.com>
2023-11-08 22:28:47 +0200

    CloudFirestore Load

--- Snacktacular/View/ListView.swift
+++ Snacktacular/View/ListView.swift
@@ -7,42 +7,53 @@
 
 import SwiftUI
 import Firebase
+import FirebaseFirestoreSwift
 
 struct ListView: View {
+    @FirestoreQuery(collectionPath: "spots") var spots: [Spot]
     @State private var sheetIsPresenting = false
     @Environment (\.dismiss) private var dismiss
     
     var body: some View {
-        List {
-            Text("List items will go here")
-        }
-        .listStyle(.plain)
-        .navigationBarBackButtonHidden()
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button("Sign Out") {
-                    do {
-                        try Auth.auth().signOut()
-                        print("Log out succesful!")
-                        dismiss()
-                    } catch {
-                        print("ERROR: Could not sign out!")
+        NavigationStack {
+            List(spots) { spot in
+                NavigationLink {
+                    SpotDetailView(spot: spot)
+                } label: {
+                    Text(spot.name)
+                        .font(.title2)
+                }
+
+            }
+            .listStyle(.plain)
+            .navigationTitle("Snack Spots")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Sign Out") {
+                        do {
+                            try Auth.auth().signOut()
+                            print("Log out succesful!")
+                            dismiss()
+                        } catch {
+                            print("ERROR: Could not sign out!")
+                        }
                     }
                 }
-            }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    sheetIsPresenting.toggle()
-                } label: {
-                    Image(systemName: "plus")
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        sheetIsPresenting.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    
                 }
-                
             }
+            .sheet(isPresented: $sheetIsPresenting) {
+                NavigationStack {
+                    SpotDetailView(spot: Spot())
+                }
         }
-        .sheet(isPresented: $sheetIsPresenting) {
-            NavigationStack {
-                SpotDetailView(spot: Spot())
-            }
         }
     }
 }
--- Snacktacular/View/LoginView.swift
+++ Snacktacular/View/LoginView.swift
@@ -19,90 +19,84 @@ struct LoginView: View {
     @State private var showingAlert = false
     @State private var alertMessage = ""
     @State private var buttonsDisabled = true
-    @State private var path = NavigationPath()
+    @State private var presentSheet = false
     @FocusState private var focusField: Field?
     
     
     var body: some View {
-        NavigationStack(path: $path) {
-            VStack {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                
-                Group {
-                    TextField("E-mail", text: $email)
-                        .keyboardType(.emailAddress)
-                        .autocorrectionDisabled()
-                        .textInputAutocapitalization(.never)
-                        .submitLabel(.next)
-                        .focused($focusField, equals: .email) ///this fild is bound to the .email case
-                        .onSubmit {
-                            focusField = .password
-                        }
-                        .onChange(of: email) { _ in
-                             enableButtons()
-                        }
-                    SecureField("Password", text: $password)
-                        .textInputAutocapitalization(.never)
-                        .submitLabel(.done)
-                        .focused($focusField, equals: .password) ///this fild is bound to the .password case
-                        .onSubmit {
-                            focusField = nil // will dismiss the keyboard
-                        }
-                        .onChange(of: password) { _ in
-                            enableButtons()
-                        }
-                }
-                .textFieldStyle(.roundedBorder)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(.gray.opacity(0.5), lineWidth: 2)
-                }
-                .padding(.horizontal)
-                
-                HStack {
-                    Button {
-                        register()
-                    } label: {
-                        Text("Sing Up")
+        VStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            Group {
+                TextField("E-mail", text: $email)
+                    .keyboardType(.emailAddress)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.next)
+                    .focused($focusField, equals: .email) ///this fild is bound to the .email case
+                    .onSubmit {
+                        focusField = .password
                     }
-                    .padding(.trailing)
-                    
-                    
-                    Button {
-                        login()
-                    } label: {
-                        Text("Log In")
+                    .onChange(of: email) { _ in
+                        enableButtons()
                     }
-                    .padding(.leading)
-
-                }
-                .disabled(buttonsDisabled)
-                .buttonStyle(.borderedProminent)
-                .tint(Color("SnackColor"))
-                .font(.title2)
-                .padding(.top)
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationDestination(for: String.self) { view in
-                    if view == "ListView" {
-                        ListView()
+                SecureField("Password", text: $password)
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.done)
+                    .focused($focusField, equals: .password) ///this fild is bound to the .password case
+                    .onSubmit {
+                        focusField = nil // will dismiss the keyboard
                     }
-                }
+                    .onChange(of: password) { _ in
+                        enableButtons()
+                    }
             }
+            .textFieldStyle(.roundedBorder)
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(.gray.opacity(0.5), lineWidth: 2)
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Button {
+                    register()
+                } label: {
+                    Text("Sing Up")
+                }
+                .padding(.trailing)
+                
+                
+                Button {
+                    login()
+                } label: {
+                    Text("Log In")
+                }
+                .padding(.leading)
+                
+            }
+            .disabled(buttonsDisabled)
+            .buttonStyle(.borderedProminent)
+            .tint(Color("SnackColor"))
+            .font(.title2)
+            .padding(.top)
             
         }
         .alert(alertMessage, isPresented: $showingAlert) {
             Button("OK", role: .cancel) {}
         }
-       
         .onAppear {
             //if logged in when app runs, navigate to the new screen & skip login screen
             if Auth.auth().currentUser != nil {
                 print("Login Successful")
-                path.append("ListView")
+                presentSheet = true
             }
+        }
+        .fullScreenCover(isPresented: $presentSheet) {
+            ListView()
         }
     }
     
@@ -122,7 +116,7 @@ struct LoginView: View {
                 showingAlert = true
             } else {
                 print("Registration success!")
-                path.append("ListView")
+                presentSheet = true
             }
         }
     }
@@ -135,7 +129,7 @@ struct LoginView: View {
                 showingAlert = true
             } else {
                 print("Login success!")
-                path.append("ListView")
+                presentSheet = true
             }
         }
     }
